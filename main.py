bc773baafbe8d647d7023d3a003ae66ca247641b
TomasGlgg <globistomas@gmail.com>
2020-12-16 20:40:03 +0300

    Setup VKLogin class

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from cmd import Cmd
-
+from os import listdir
+from user import User
 
 class VKLogin(Cmd):
     token_list = []
+
+    def preloop(self):
+        Cmd.preloop(self)
+        self.load_token_list()
+
+    def save_token_list(self):
+        file = open('tokens.txt', 'w')
+        for token in self.token_list:
+            file.write(token+'\n')
+
+    def load_token_list(self):
+        if 'tokens.txt' not in listdir(): return
+        file = open('tokens.txt', 'r')
+        for line in file.readlines():
+            self.token_list.append(line.strip())
+        print('Список токенов загружен')
+
+    # Commands
 
     def do_add(self, argv):
         if len(argv.split()) != 1:
             print("Неправильное количество аргументов")
             return
         self.token_list.append(argv.split()[0])
+        self.save_token_list()
         print('Добавлено')
         return
 
@@ -19,8 +39,29 @@ class VKLogin(Cmd):
         self.token_list.remove(argv.split()[0])
 
     def do_list(self, argv):
+        id = 1
         for token in self.token_list:
-            print(token)
+            print(index, token)
+            id += 1
+
+    def do_auth(self, argv):
+        '''
+        auth <token index>
+        '''
+        if len(argv.split()) != 1:
+            print("Неправильное количество аргументов")
+            return
+
+        token_id = int(argv.split()[0])
+        if token_id>len(self.token_list):
+            print('Токен не найден')
+            return
+        token = self.token_list[token_id]
+        user = User()
+        user.load_token(token)
+        user.auth()
+        user.setup()  # setup settings (banner, prompt)
+        user.cmdloop()
 
 
 if __name__ == '__main__':
