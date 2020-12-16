4e9e7a197c2fe72317da15254a4ecdc9510f2121
assasinfil <nfa57643@gmail.com>
2020-12-16 21:52:12 +0300

    File IO fix
    Signed-off-by: assasinfil <nfa57643@gmail.com>

--- main.py
+++ main.py
@@ -1,60 +1,72 @@
 from cmd import Cmd
 from os import listdir
+
 from user import User
 
+
 class VKLogin(Cmd):
-    token_list = []
+    tokens = []
 
     def preloop(self):
         Cmd.preloop(self)
-        self.load_token_list()
+        self.load_tokens()
 
     def save_token_list(self):
-        file = open('tokens.txt', 'w')
-        for token in self.token_list:
-            file.write(token+'\n')
+        with open('tokens.txt', 'w') as f:
+            for token in self.tokens:
+                f.write(token + '\n')
 
-    def load_token_list(self):
-        if 'tokens.txt' not in listdir(): return
-        file = open('tokens.txt', 'r')
-        for line in file.readlines():
-            self.token_list.append(line.strip())
+    def load_tokens(self):
+        if 'tokens.txt' not in listdir():
+            return
+        with open('tokens.txt', 'r') as f:
+            for line in f.readlines():
+                self.tokens.append(line.strip())
         print('Список токенов загружен')
 
     # Commands
 
     def do_add(self, argv):
+        """
+        usage: add <token>
+        """
         if len(argv.split()) != 1:
             print("Неправильное количество аргументов")
             return
-        self.token_list.append(argv.split()[0])
+        self.tokens.append(argv.split()[0])
         self.save_token_list()
         print('Добавлено')
         return
 
     def do_delete(self, argv):
+        """
+        usage: delete <token>
+        """
         if len(argv.split()) != 1:
             print("Неправильное количество аргументов")
             return
-        self.token_list.remove(argv.split()[0])
+        self.tokens.remove(argv.split()[0])
 
     def do_list(self, argv):
-        for id, token in enumerate(self.token_list):
-            print(id, token[:10]+'...')
+        """
+        usage: list
+        """
+        for i, token in enumerate(self.tokens):
+            print(i, token[:10] + '...')
 
     def do_auth(self, argv):
-        '''
-        auth <token index>
-        '''
+        """
+        usage: auth <token index>
+        """
         if len(argv.split()) != 1:
             print("Неправильное количество аргументов")
             return
 
         token_id = int(argv.split()[0])
-        if token_id>len(self.token_list):
+        if token_id > len(self.tokens):
             print('Токен не найден')
             return
-        token = self.token_list[token_id]
+        token = self.tokens[token_id]
         user = User()
         user.load_token(token)
         user.auth()
