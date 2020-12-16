85626a41085b91b958e6935ee0572e4ac6fc9b64
assasinfil <nfa57643@gmail.com>
2020-12-16 21:50:52 +0300

    Added commands info
    Signed-off-by: assasinfil <nfa57643@gmail.com>

--- user.py
+++ user.py
@@ -1,22 +1,21 @@
 from cmd import Cmd
-from parse_functions import Parser
+
 import vk
+
+from parse_functions import Parser
 
 
 class User(Cmd):
     def load_token(self, token):
         self.token = token
 
-
     def auth(self):
         session = vk.Session(self.token)
         self.api = vk.API(session)
         self.parser = Parser(self.api)
 
-
     def setup(self):
         self.profile_info = self.api.account.getProfileInfo(v=5.126)
-
 
         # setup prompt
         self.prompt = '({} {})>'.format(self.profile_info['first_name'], self.profile_info['last_name'])
@@ -30,13 +29,13 @@ class User(Cmd):
         '''
 
     def do_dialogs(self, argv):
-        if len(argv.split()) != 1:
+        if len(argv.split()) > 1:
             print('Неверное количество аргументов')
             return
         count = argv.split()[0]
+        if count is None:
+            count = 10
         messages = self.api.messages.getConversations(count=count, v='5.52')['items']
         for message in messages:
             self.parser.show_message(message)
-            print('-'*20)
-
-
+            print('-' * 20)
