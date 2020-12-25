76f9c70be4b534f8b1b14d73e9ea728efb43800d
TomasGlgg <globistomas@gmail.com>
2020-12-25 23:47:57 +0300

    Added read chat dialog

--- messages/chat_dialog.py
+++ messages/chat_dialog.py
@@ -6,7 +6,7 @@ from messages.messages_parser import Chat_messages_parser
 
 class Chat_dialog(Dialog):
     def setupUI(self):
-        self.parser = Chat_messages_parser(self.api, self.chat_id)
+        self.parser = Chat_messages_parser(self.api, self.chat_id, self.profile_info)
         self.chat_info = self.api.messages.getChat(chat_id=self.chat_id - 2000000000, v=5.126)
         self.prompt = '({} {})->({})>'.format(self.profile_info['first_name'], self.profile_info['last_name'],
                                               self.chat_info['title'])
@@ -15,3 +15,13 @@ class Chat_dialog(Dialog):
         Название чата: {self.chat_info['title']}
         Количество учатсников: {colored(self.chat_info['members_count'], 'cyan')}
         '''
+
+    def do_read(self, argv):
+        """
+        usage: read [count]
+        """
+        if len(argv.split()) == 0:
+            count = 10
+        else:
+            count = int(argv.split()[0])
+        self.parser.print_last_messages(count)
--- messages/messages_parser.py
+++ messages/messages_parser.py
@@ -16,6 +16,7 @@ def _print_private_message(message):
         print()
 
 
+
 class Private_messages_parser:
     def __init__(self, api, peer_id):
         self.peer_id = peer_id
@@ -26,10 +27,24 @@ class Private_messages_parser:
         for message in messages[::-1]:
             _print_private_message(message)
 
+
 class Chat_messages_parser:
-    def __init__(self, api, chat_id):
+    def __init__(self, api, chat_id, profile_id):
         self.peer_id = chat_id
         self.api = api
+        self.profile_id = profile_id
+
+    def _print_chat_message(self, message):
+        date = datetime.fromtimestamp(message['date'])
+        print('--------', date.strftime('%Y-%m-%d %H:%M:%S'))
+        if message['from_id'] == self.profile_id['id']:
+            print('Сообщение', colored('(Вы):', 'green'), end=' ')
+        else:
+            peer_info = self.api.users.get(user_ids=[message['from_id']], v=5.126, name_case='gen')[0]
+            print('Сообщение от', colored(peer_info['first_name'] + ' ' + peer_info['last_name'], 'red') + ':', end=' ')
+        print(message['text'])
 
     def print_last_messages(self, count):
-        pass
+        messages = self.api.messages.getHistory(peer_id=self.peer_id, count=count, v=5.126)['items']
+        for message in messages[::-1]:
+            self._print_chat_message(message)
