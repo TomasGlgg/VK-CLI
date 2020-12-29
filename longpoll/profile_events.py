da54ee4201d5b986d87d08156ff978e28c30bca4
TomasGlgg <globistomas@gmail.com>
2020-12-29 16:57:59 +0300

    Added print message id

--- longpoll/profile_events.py
+++ longpoll/profile_events.py
@@ -30,7 +30,7 @@ class Profile_events:
             chat_title = chat_info['title']
             self.chats[event.chat_id] = chat_title
 
-        print(self.chats[event.chat_id], end='')
+        print(self.chats[event.chat_id], end=' ')
 
     def _print_message(self, event):
         print('----------', colored('Новое сообщение:', 'red'))
@@ -42,12 +42,13 @@ class Profile_events:
 
         if event.from_user:
             self._print_cache_user(event)
-            print()
+            print('- №' + str(event.message_id))
 
         elif event.from_chat:
             self._print_cache_user(event)
             print('в беседе')
             self._print_cache_chat(event)
+            print('- №' + str(event.message_id))
 
         elif event.from_group:
             print('группы', event.group_id)  # TODO
@@ -59,4 +60,3 @@ class Profile_events:
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
                 self._print_message(event)
-            # TODO: more events
